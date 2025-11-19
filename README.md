1c866e6208a2427151767318e62aa052eeb1ebb5
epc9020 <epp9020@outlook.com>
2025-11-19 14:41:54 -0400

    oh shit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# the site of all time
 Hai hai! im... elite! i have a website, on neocities! i use github for easy updating... my neoccities site!
 
 there i made a readme file you happy github?
+(if you're interested, all files are in the "public" directory)
---
+++ scripts/statuscafe.js
@@ -0,0 +1,13 @@
+// status.cafe script obviously made by status.cafe, i modified it to my liking
+document.writeln('<div id="statuscafe-username"></div><div id="statuscafe-content"></div></div>');
+fetch("https://status.cafe/users/eliteproot784/status.json")
+  .then( r => r.json() )
+  .then( r => {
+    if (!r.content.length) {
+      document.getElementById("statuscafe-content").innerHTML = "empty thought"
+      return
+    }
+    document.getElementById("statuscafe-username").innerHTML = '<a href="https://status.cafe/users/eliteproot784" target="_blank">' + "elite784" + '</a> ' + "says:"
+    document.getElementById("statuscafe-content").innerHTML = r.content + "    " + r.face
+    document.getElementById("statuscafe-timeAgo").innerHTML = ' (' + r.timeAgo + ')'
+  })
